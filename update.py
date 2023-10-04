6cc76a0640cb68c5a75f8f4a1a7b181fcc52a4b4
GitXBot101 <117470125+GitXBot101@users.noreply.github.com>
2023-10-04 13:32:15 +0530

    Update update.py

--- update.py
+++ update.py
@@ -33,7 +33,7 @@ if DATABASE_URL:
 
 UPSTREAM_BRANCH = 'jmdkh'
 
-if UPSTREAM_REPO := 'https://github.com/5hojib/Luna':
+if UPSTREAM_REPO := 'https://github.com/GitXBot101/jmdkh-luna':
     if ospath.exists('.git'):
         srun(["rm", "-rf", ".git"])
 
